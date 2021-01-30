97dca0936fe1c03a574e2e83f00e499d4cdc53b6
Maksym Serebrennykov <73495553+accmax@users.noreply.github.com>
2021-01-30 20:01:41 +0200

    добавил секцию продукты

---
+++ src/js/slider.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+	$('.review-slider').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:1,
+		autoplay:true,
+		speed:1000,
+		autoplaySpeed:2000
+	});
+// });
+
