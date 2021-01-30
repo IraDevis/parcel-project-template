8cbfa91dc5fcea5e8f554f03436adab71340d341
Zhdanov Yevhen <jeka7753191@gmail.com>
2021-01-30 21:44:31 +0200

    Полностью сделана секция page-footer

---
+++ src/js/slider.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+	$('.review-slider').slick({
+		arrows:false,
+		dots:true,
+		slidesToShow:1,
+		autoplay:true,
+		speed:1000,
+		autoplaySpeed:2000
+	});
+// });
