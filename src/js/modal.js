83c98500bfba58ecf53d257c028dcf2bc1a9e605
Kolinya <74129726+Kolinya@users.noreply.github.com>
2021-02-01 17:55:11 +0200

    add upButton an close burger

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,11 +2,13 @@
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
+     closeModalLink: document.querySelector('[burger-menu-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalLink.addEventListener('click', toggleModal);
 
   function toggleModal() {
     document.body.classList.toggle('modal-open')
